9d8dd61ee3f1811a5410404170f632b522ae0038
Michael Villanueva Balanzar <michael.villanueva.b91@gmail.com>
2025-09-21 14:37:13 -0600

    se agrego permisos a los endpoints de category

--- categories/api/views.py
+++ categories/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.api.serializers import CategorySerializer
+from categories.api.permisions import IsAdminOrReadOnly
 
 class CategoryApiViewSet(ModelViewSet):
+  permission_classes = [IsAdminOrReadOnly]
   queryset = Category.objects.all()
   serializer_class = CategorySerializer
