c8d5fca7b4a32687c419ae90c7eafb55f53403de
michael villanueva balanzar <michael.villanueva.b91@gmail.com>
2025-09-22 13:06:57 -0600

    se agrego el fintrado de los post por categoria tomando en cuenta el id de la categoria o el slug de la muisma

--- post/api/views.py
+++ post/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from post.models import Post
 from post.api.serializers import PostSerializer
 from post.api.permissions import IsAdminOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 class PostApiViewSet(ModelViewSet):
     #queryset = Post.objects.all()
@@ -9,3 +10,7 @@ class PostApiViewSet(ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = [IsAdminOrReadOnly]
     # lookup_field = 'slug' # para buscar por slug en vez de id
+    filter_backends = [DjangoFilterBackend]
+    #filterset_fields = ['category'] # filtrar por la id de categoria del post http://127.0.0.1:8000/api/posts/?category=1
+    filterset_fields = ['category','category__slug'] # filtrar por el slug de categoria del post http://127.0.0.1:8000/api/posts/?category__slug=react
+    
