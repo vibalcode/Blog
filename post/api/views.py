f0e704dfa616875df7fed1ddcb8e7b4158e07430
michael villanueva balanzar <michael.villanueva.b91@gmail.com>
2025-09-22 10:50:25 -0600

    se agregoaron los permisos para los post

--- post/api/views.py
+++ post/api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from post.models import Post
 from post.api.serializers import PostSerializer
+from post.api.permissions import IsAdminOrReadOnly
 
 class PostApiViewSet(ModelViewSet):
     #queryset = Post.objects.all()
     queryset = Post.objects.filter(published=True)
     serializer_class = PostSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    # lookup_field = 'slug' # para buscar por slug en vez de id
